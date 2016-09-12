56dff349e816f3ef8e6297dee672a7040093f99a
Rover <rover@ustream.tv>
2016-09-12 17:16:14 +0200

    Modifications has been made

--- src/index.js
+++ src/index.js
@@ -1,64 +1,52 @@
 import * as utils from './utils';
 
-// https://gist.github.com/Xeoncross/7663273
+function open(x, url, data) {
+  let method = 'GET';
+  if (data) {
+    method = 'POST';
+  }
+  x.open(method, url, 1);
+}
+
 function ajax(url, options, callback, data, cache) {
-  // Must encode data
-  if(data && typeof data === 'object') {
-    var y = '', e = encodeURIComponent;
-    for (var m in data) {
-      y += '&' + e(m) + '=' + e(data[m]);
+  const dataObject = (data === 'object');
+  const isValid = data && dataObject;
+  let workData = data && JSON.parse(JSON.stringify(data));
+  if (isValid) {
+    let y = '';
+    workData.forEach((m) => {
+      y += `&${encodeURIComponent(m)}=${encodeURIComponent(workData[m])}`;
+    });
+    workData = y.slice(1);
+    if (!cache) {
+      workData += `&_t=${new Date}`;
     }
-    data = y.slice(1) + (!cache ? '&_t=' + new Date : '');
   }
 
   try {
-    var x = new (XMLHttpRequest || ActiveXObject)('MSXML2.XMLHTTP.3.0');
-    x.open(data ? 'POST' : 'GET', url, 1);
+    const x = new (XMLHttpRequest || ActiveXObject)('MSXML2.XMLHTTP.3.0'); // eslint-disable-line no-undef
+    open(x, url, workData);
     if (!options.crossDomain) {
       x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     }
     if (data) {
       x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     }
-    x.onreadystatechange = function() {
-      x.readyState > 3 && callback && callback(x.responseText, x);
+    x.onreadystatechange = function onReadyStateChange() {
+      const correctState = x.readyState > 3;
+      if (correctState && callback) {
+        return callback(x.responseText, x);
+      }
+      return null;
     };
     x.send(data);
   } catch (e) {
-    window.console && console.log(e);
+    if (window.console) {
+      return console.log(e); // eslint-disable-line no-console
+    }
   }
-};
-
-// ajax.uriEncode = function(o) {
-//     var x, y = '', e = encodeURIComponent;
-//     for (x in o) y += '&' + e(x) + '=' + e(o[x]);
-//     return y.slice(1);
-// };
-//
-// ajax.collect = (a, f) {
-//     var n = [];
-//     for (var i = 0; i < a.length; i++) {
-//         var v = f(a[i]);
-//         if (v != null) n.push(v);
-//     }
-//     return n;
-// };
-//
-// ajax.serialize = function (f) {
-//     function g(n) {
-//         return f.getElementsByTagName(n);
-//     };
-//     var nv = function (e) {
-//         if (e.name) return encodeURIComponent(e.name) + '=' + encodeURIComponent(e.value);
-//     };
-//     var i = collect(g('input'), function (i) {
-//         if ((i.type != 'radio' && i.type != 'checkbox') || i.checked) return nv(i);
-//     });
-//     var s = collect(g('select'), nv);
-//     var t = collect(g('textarea'), nv);
-//     return i.concat(s).concat(t).join('&');
-// };
-//
+  return null;
+}
 
 function getDefaults() {
   return {
@@ -67,7 +55,7 @@ function getDefaults() {
     allowMultiLoading: false,
     parse: JSON.parse,
     crossDomain: false,
-    ajax: ajax
+    ajax
   };
 }
 
@@ -84,54 +72,62 @@ class Backend {
   }
 
   readMulti(languages, namespaces, callback) {
-    var loadPath = this.options.loadPath;
+    let loadPath = this.options.loadPath;
     if (typeof this.options.loadPath === 'function') {
-	    loadPath = this.options.loadPath(languages, namespaces);
+      loadPath = this.options.loadPath(languages, namespaces);
     }
 
-    let url = this.services.interpolator.interpolate(loadPath, { lng: languages.join('+'), ns: namespaces.join('+') });
+    const url = this.services.interpolator.interpolate(loadPath, { lng: languages.join('+'), ns: namespaces.join('+') });
 
     this.loadUrl(url, callback);
   }
 
   read(language, namespace, callback) {
-    var loadPath = this.options.loadPath;
+    let loadPath = this.options.loadPath;
     if (typeof this.options.loadPath === 'function') {
-	    loadPath = this.options.loadPath([language], [namespace]);
+      loadPath = this.options.loadPath([language], [namespace]);
     }
 
-    let url = this.services.interpolator.interpolate(loadPath, { lng: language, ns: namespace });
+    const url = this.services.interpolator.interpolate(loadPath, { lng: language, ns: namespace });
 
     this.loadUrl(url, callback);
   }
 
   loadUrl(url, callback) {
     this.options.ajax(url, this.options, (data, xhr) => {
-      if (xhr.status >= 500 && xhr.status < 600) return callback('failed loading ' + url, true /* retry */);
-      if (xhr.status >= 400 && xhr.status < 500) return callback('failed loading ' + url, false /* no retry */);
+      if (xhr.status >= 500 && xhr.status < 600) {
+        return callback(`failed loading ${url}`, true /* retry */);
+      }
+      if (xhr.status >= 400 && xhr.status < 500) {
+        return callback(`failed loading ${url}`, false /* no retry */);
+      }
 
-      let ret, err;
+      let ret;
       try {
         ret = this.options.parse(data, url);
       } catch (e) {
-        err = 'failed parsing ' + url + ' to json';
+        const err = `failed parsing ${url} to json`;
+        return callback(err, false);
       }
-      if (err) return callback(err, false);
-      callback(null, ret);
+      return callback(null, ret);
     });
   }
 
-  create(languages, namespace, key, fallbackValue) {
-    if (typeof languages === 'string') languages = [languages];
+  create(recievedLanguages, ns, key, fallbackValue) {
+    let languages = recievedLanguages;
+    if (typeof recievedLanguages === 'string') {
+      languages = [recievedLanguages];
+    }
 
-    let payload = {};
+    const payload = {};
     payload[key] = fallbackValue || '';
 
-    languages.forEach(lng => {
-      let url = this.services.interpolator.interpolate(this.options.addPath, { lng: lng, ns: namespace });
+    languages.forEach((lng) => {
+      const interpolater = { lng, ns };
+      const url = this.services.interpolator.interpolate(this.options.addPath, interpolater);
 
-      this.options.ajax(url, this.options, function(data, xhr) {
-        //const statusCode = xhr.status.toString();
+      this.options.ajax(url, this.options, (data, xhr) => {
+        // const statusCode = xhr.status.toString();
         // TODO: if statusCode === 4xx do log
       }, payload);
     });
--- src/utils.js
+++ src/utils.js
@@ -1,25 +1,30 @@
-let arr = [];
-let each = arr.forEach;
-let slice = arr.slice;
+const arr = [];
+const each = arr.forEach;
 
-export function defaults(obj) {
-  each.call(slice.call(arguments, 1), function(source) {
-    if (source) {
-      for (var prop in source) {
-        if (obj[prop] === undefined) obj[prop] = source[prop];
+export function defaults(obj, ...args) {
+  each.call(args, (source) => {
+    if (!source) {
+      return;
+    }
+    for (const prop in source) { // eslint-disable-line no-restricted-syntax
+      if (obj[prop] === undefined) {
+        obj[prop] = source[prop]; // eslint-disable-line no-param-reassign
       }
     }
   });
   return obj;
 }
 
-export function extend(obj) {
-  each.call(slice.call(arguments, 1), function(source) {
-    if (source) {
-      for (var prop in source) {
-        obj[prop] = source[prop];
-      }
+export function extend(obj, ...args) {
+  each.call(args, (source) => {
+    if (source === undefined) {
+      return;
     }
+    /* eslint-disable */
+    for (const prop in source) {
+      obj[prop] = source[prop];
+    }
+    /* eslint-enable */
   });
   return obj;
 }
